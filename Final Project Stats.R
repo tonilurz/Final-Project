8d801f314d745d32e10f8fadcff47f3119144c47
Toni Lurz <toni.lurz@gmail.com>
2019-12-09 15:29:39 -0600

    This is the Start of the Stepwise Regression

--- Final Project Stats.R
+++ Final Project Stats.R
@@ -1,6 +1,34 @@
-ChiData1 <- select(ChicagoPublicSchools,`Name of School`, `ZIP Code`, `CPS Performance Policy Level`, `Supportive Environment`,`Effective Leaders`,`Ambitious Instruction`, `Collaborative Teachers`, `Safe`, `School Community`,`Quality of Facilities`,
-             `NWEA Math Growth Percentile All Grades`, `NWEA Reading Growth Percentile All Grades`,`NWEA Math Attainment Percentile All Grades`, `NWEA Reading Attainment Percentile All Grades`)
+library("car")
+library("caret")
+library("gvlma")
+library("predictmeans")
+library("psych")
+library("Hmisc")
 
-# Histogram for the start of exploration
-CPSH <- ggplot(ChiData1, aes(x =`NWEA Math Growth Percentile All Grades`))
-CPSH + geom_histogram()
+# Data Wrangle and Transformation
+summary(ChicagoPublicSchools)
+
+#New Data Set for Analysis
+ChiData1 <- subset(ChicagoPublicSchools,select = c(`School ID`, `ZIP Code`, `CPS Performance Policy Level`, `Supportive Environment`,`Effective Leaders`,`Ambitious Instruction`, `Safe`, `School Community`,`Quality of Facilities`,
+             `NWEA Math Growth Percentile All Grades`, `NWEA Reading Growth Percentile All Grades`,`NWEA Math Attainment Percentile All Grades`, `NWEA Reading Attainment Percentile All Grades`,`Suspensions Per 100 students 2013`,`Student Attendance Percentage 2013`,
+             `Grade 3-8 On-Track Percentage 2013`))
+# Preparing Data for Regression Models
+str(ChiData1)
+ChiData1$`CPS Performance Policy Level`= as.numeric(ChiData1$`CPS Performance Policy Level`)
+ChiData1$`Supportive Environment`= as.numeric(ChiData1$`Supportive Environment`)
+ChiData1$`Effective Leaders` = as.numeric(ChiData1$`Effective Leaders`)
+ChiData1$`Ambitious Instruction`= as.numeric(ChiData1$`Ambitious Instruction`)
+ChiData1$`Safe`= as.numeric(ChiData1$`Safe`)
+ChiData1$`School Community`= as.numeric(ChiData1$`School Community`)
+ChiData1$`Quality of Facilities`= as.numeric(ChiData1$`Quality of Facilities`)
+str(ChiData1)
+na.omit(ChiData1)
+
+# Backwards Stepwise Regression Model
+FitAll = lm(`CPS Performance Policy Level` ~ ., data = ChiData1)
+summary(FitAll)
+
+
+
+
+
